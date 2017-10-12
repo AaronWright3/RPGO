923feb4cf4dfdc39180b22b0a1c02a6c9594c7ad
Aaron Wright <aaronwright3@yahoo.com>
2017-10-11 23:15:48 -0700

    version 0.3: now supports multiple games, character creation, game creation, setting a nickname, and a functional chat

--- serverCore.js
+++ serverCore.js
@@ -1,68 +1,157 @@
 var clientList = {}; // object containing clients
-var game = null; // will eventually be an object containing the game info
+var gameList = {};
 
-window.addEventListener("message", receiveMessage, false); //waits for messages and calls receiveMessage when it receives one, passing on the event as its parameters
-
-function receiveMessage(event) { //accepts the message event as parameters and deals with the message accordingly
-	var m = JSON.parse(event.data);
-	
-	if (m.type == "status" && m.data == "init") { //if the client sends the server a status message saying that it wishes to initialize
-		clientList[m.name] = { //creates object in client list with the client's name and the following properties
-			name: m.name, //unique ID
-			location: "lobby", //current location
-			lastMessage: m.data.sent, //most recent message date
-			source: event.source //source, used as a target when sending messages
-		}; 
-		
-		console.log("initialized %c" + m.name, "color: #" + m.name); //prints to the console "initialized {clientName}" with {clientName} being uniquely colored.
-		
-		if (game === null) { //if no game exists
-			createGame(m.name); //I think you know what this does
-		} else {
-			game.clientList.push(clientList[m.name].name); //if a game does exist, add this new client to the game
-		}
-		cm("joinGame", game.ID, m.name); //tell the client which game it just joined 
-	}
-	
-	if (clientList[m.name]) { //if the client is already on the client list
-		clientList[m.name].lastMessage = m.data.sent; //set its last message date to this one
-		
-		if (m.type == "chatMessage") { //if it's a chat message
-			var newMessage = "<div class='message'><div class='chatname' style='color: #" + m.name + "'>" + m.name + ":</div><div class='messagebody'>" + m.data + "</div></div>"; //format chat item
-			console.log(m.name + ": %c" + m.data, "color: #" + m.name); //log to console
-			game.chat.push(newMessage); //add to chat message array
-			document.getElementById("console").innerHTML = game.chat.join("<br>"); //update current chat display
-			broadcast("chatMessage", newMessage); //send latest message to all clients
-		}
-		
-		if (m.type == "request") {
-			console.log(m.name + " requested game data from game " + m.data);
-			updateGame(); //now that the client has joined, update connected clients with current game data
-		}
-		
-		if (m.type == "status" && m.data == "close") { //if the client wants to close its connection
-			delete clientList[m.name]; //delete client from list
-			game.clientList.splice(game.clientList.indexOf(m.name), 1) // deletes client from game info
-			console.log("closed %c" + m.name, "color: #" + m.name);
-			updateGame();
-		}
-	}
+function randomHex() {
+	return ("000000" + Math.floor(Math.random()*16777215).toString(16)).substr(-6);
 }
+
+function augment(obj, properties){
+    for (var key in properties){
+        obj[key] = properties[key]
+    }
+}
+
+function game(ID, name, creator, rules) { //creates a game
+	this.name = name;
+	this.ID = ID;
+	this.host = creator;
+	this.clientList = [];
+	this.status = "setup";
+	this.publicProperties = {
+		round: 0,
+		turn: 0,
+		space: 0,
+	};
+	this.privateProperties = {};
+	this.chat = [];
+	this.party = {
+			characters: {},
+			publicProperties: {},
+			privateProperties: {},
+		};
+	this.rules = rules;
+	this.getData = function(){ //returns client-friendly game data
+		return {
+			name: this.name,
+			ID: this.ID,
+			clientList: this.clientList,
+			status: this.status,
+			properties: this.publicProperties,
+			chat: this.chat,
+			party: {
+				characters: this.party.characters,
+			},
+			rules: this.rules,
+		}
+	};
+}
+
+function createGame(name, rules, creator) {
+	var ID = randomHex();
+	gameList[ID] = new game(ID, name, creator, rules)
+	return ID;
+}
+
+
+window.addEventListener("message", receiveMessage, false);
+
 
 function cm(type, message, target) { //sends message to a specific client
 	var packet = {
-		name: target,
+		name: clientList[target].name,
+		location: clientList[target].location,
+		ID: target,
 		type: type,
 		data: message,
 		sent: new Date().getTime(),
 	}
+	console.log("packet:", packet);
 	var JSONpacket = JSON.stringify(packet); //puts data in JSON format
 	clientList[target].source.postMessage(JSONpacket, "*"); //sends JSON-ified message to specific client
 }
 
+function receiveMessage(event) { //accepts the message event as parameters and deals with the message accordingly
+	var m = JSON.parse(event.data);
+	console.log(m);
+	function addClient() {
+		var privateID = randomHex(); //generates unique ID for the client
+		clientList[privateID] = { //creates object in client list with the client's name and the following properties
+			name: m.name, //internal client name
+			nickname: m.nickname,
+			ID: privateID, // unique ID
+			game: null,
+			location: "lobby",
+			lastMessage: m.data.sent, //most recent message date
+			source: event.source, //source, used as a target when sending messages
+			joinTime: m.data.sent,
+		}; 
+		console.log("initialized %c" + m.name, "color: #" + m.name);
+		return privateID;
+	}
+	
+	if (m.type == "init" && m.data == m.nickname) {
+		var newID = addClient();
+		cm("gameList", gameList, newID);
+	}
+	
+	if (clientList[m.ID]) { //if the client is already on the client list
+		clientList[m.ID].lastMessage = m.data.sent; //set its last message date to this one
+		
+		if (m.type == "createGame") {
+			gameID = createGame(m.data.name, m.data.rules, m.ID);
+			cm("joinGame", gameID, m.ID);
+		}
+		
+		if (m.type == "joinGame" && gameList[m.data]) {
+			cm("joinGame", gameID, m.ID);
+		}
+		
+		if (gameList[m.game]) { //if referenced game actually exists
+			var game = gameList[m.game];
+			if (m.type == "characterData") { //if the client sends the server a status message saying that it wishes to join
+				console.log("characterData");
+					if (game.clientList.indexOf(m.name) == -1) {
+						if ((game.status == "setup" && game.host == m.ID) || game.status == "lobby") { //|| game.status == "lobby") && game.clientList.indexOf(m.name) == -1
+							game.clientList.push(m.name);
+							game.party.characters[m.name] = m.data;
+							clientList[m.ID].location = game.ID;
+							if (game.status = "lobby") {
+								updateGame(game);
+							};
+							if (game.status == "setup") {
+								game.status = "lobby";
+							}
+							cm("gameData", game.getData(), m.ID);
+						}
+					} else if (game.status == "running") {
+						cm("error", "game is already in progress", m.ID)
+					} else if (game.clientList.indexOf(m.name) !== -1) {
+						cm("gameData", game.getData(), m.ID);
+					}
+			}
+		
+			if (m.type == "chatMessage") { //if it's a chat message
+			var newMessage = "<div class='message'><div class='chatname' style='color: " + game.party.characters[clientList[m.ID].name].color + "'>" + clientList[m.ID].nickname + " (" + game.party.characters[clientList[m.ID].name].name + "):</div><div class='messagebody'>" + m.data + "</div></div>"; //format chat item
+				console.log(+ clientList[m.ID].nickname + " (" + game.party.characters[clientList[m.ID].name].name + "): %c" + m.data, "color: " + game.party.characters[clientList[m.ID].name].color ); //log to console
+				addToChat(game, newMessage);
+			}
+		}
+		
+		if (m.type == "status" && m.data == "close") { //if the client wants to close its connection
+			if (gameList[m.game]) {
+				var game = gameList[m.game]
+				delete game.party.characters[clientList[m.ID].name];
+				game.clientList.splice(game.clientList.indexOf(clientList[m.ID].name), 1) // deletes client from game info
+				updateGame(game);
+			}
+			delete clientList[m.ID]; //delete client from list
+		}
+	}
+}
+
 function tellAll() { //sends text message to all clients, for testing purposes mostly
 	if (document.getElementById("textinput").value !== "") { //if the input isn't empty
-		var newMessage = "<div class='message' style='background: #000; color: #fff'><div class='chatname'>SERVER:<div class='messagebody'>" + document.getElementById("textinput").value + "</div></div>" //format message
+		var newMessage = "<div class='message' style='background: #000; color: #fff'><div class='chatname'>SERVER:<div class='messagebody'>" + document.getElementById("textinput").value + "</div></div></div>" //format message
 		document.getElementById("textinput").value = ""; //clear input field
 		game.chat.push(newMessage); //add to chat message array
 		document.getElementById("console").innerHTML = game.chat.join("<br>"); //update console
@@ -70,9 +159,24 @@ function tellAll() { //sends text message to all clients, for testing purposes m
 	}
 }
 
-function broadcast(type,data) { //sends a message to all clients
-	for (prop in clientList) {
-		cm(type, data, prop);
+function addToChat(g, message) {
+			g.chat.push(message); //add to chat message array
+			document.getElementById("console").innerHTML = g.chat.join("<br>"); //update current chat display
+			broadcast("chatMessage", message, g.ID); //send latest message to all clients
+}
+
+function broadcast(type,data, gID) { //sends a message to all clients
+	console.log(type, data, gID);
+	if (!gID) {
+		for (prop in clientList) {
+			cm(type, data, prop);
+		}
+	} else {
+		for (prop in clientList) {
+			if (gameList[gID].clientList.indexOf(clientList[prop].name) !== -1) {
+				cm(type, data, prop);
+			}
+		}
 	}
 }
 
@@ -89,21 +193,6 @@ function closeclientList() { //if the server closes, close the clients too
 
 window.onunload = closeclientList; //calls above function if window is unloaded 
 
-
-function createGame(cl) { //creates a game
-	game = {
-		ID: ("000000" + Math.floor(Math.random()*16777215).toString(16)).substr(-6), //random 6-digit hex ID, just because
-		clientList: [cl], //populates array with the creator of the game
-		status: "lobby", //you can read
-		turn: 0, //ditto
-		chat: [], // chat history
-	} 
-	return game.ID; //returning the game's ID isn't currently used for anything, but could be useful
-}
-
-function updateGame(info) { //updates game info across all clients and updates server display
-	broadcast("gameData", game);
-	document.getElementById("gametitle").innerHTML = "game title: " + game.ID;
-	document.getElementById("gamedescription").innerHTML = "game status: " + game.status;
-	document.getElementById("clientlist").innerHTML = "client list: <br>" + game.clientList.join("<br>");
+function updateGame(g, info) { //updates game info across all clients and updates server display
+	broadcast("gameData", g.getData(), g.ID);
 }
